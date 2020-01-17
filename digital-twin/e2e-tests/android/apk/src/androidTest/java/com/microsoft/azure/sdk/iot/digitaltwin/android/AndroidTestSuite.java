bf6267768b7af12a66bfac8b2eb46d676d6e4c46
timtay-microsoft <timtay@microsoft.com>
2020-01-17 14:03:50 -0800

    extends rather than suite

--- digital-twin/e2e-tests/android/apk/src/androidTest/java/com/microsoft/azure/sdk/iot/digitaltwin/android/AndroidTestSuite.java
+++ digital-twin/e2e-tests/android/apk/src/androidTest/java/com/microsoft/azure/sdk/iot/digitaltwin/android/AndroidTestSuite.java
@@ -22,9 +22,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @TestGroupDigitalTwin1
-@RunWith(Suite.class)
-@Suite.SuiteClasses( {DigitalTwinCommandE2ETests.class})
-public class AndroidTestSuite {
+public class AndroidTestSuite extends DigitalTwinCommandE2ETests {
     @Rule
     public Timeout globalTimeout = Timeout.seconds(5 * 60); // 5 minutes max per method tested
 
