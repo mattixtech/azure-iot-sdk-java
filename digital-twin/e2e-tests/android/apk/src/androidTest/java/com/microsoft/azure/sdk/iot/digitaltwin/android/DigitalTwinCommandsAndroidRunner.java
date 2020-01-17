1f038740095f1dfcf702ce11e861435981e286e9
timtay-microsoft <timtay@microsoft.com>
2020-01-17 11:54:33 -0800

    it works?

---
+++ digital-twin/e2e-tests/android/apk/src/androidTest/java/com/microsoft/azure/sdk/iot/digitaltwin/android/DigitalTwinCommandsAndroidRunner.java
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) Microsoft. All rights reserved.
+ *  Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+
+package com.microsoft.azure.sdk.iot.digitaltwin.android;
+
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
+import com.microsoft.azure.sdk.iot.digitaltwin.android.helper.TestGroupDigitalTwin1;
+import com.microsoft.azure.sdk.iot.digitaltwin.e2e.tests.DigitalTwinCommandE2ETests;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.rules.Timeout;
+
+@TestGroupDigitalTwin1
+public class DigitalTwinCommandsAndroidRunner extends DigitalTwinCommandE2ETests {
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(5 * 60); // 5 minutes max per method tested
+
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
+    @After
+    public void labelSnapshot()
+    {
+        reportHelper.label("Stopping Digital Twin E2E App");
+    }
+}
--- digital-twin/e2e-tests/android/apk/src/androidTest/java/com/microsoft/azure/sdk/iot/digitaltwin/android/DigitalTwinServiceClientAndroidRunner.java
+++ digital-twin/e2e-tests/android/apk/src/androidTest/java/com/microsoft/azure/sdk/iot/digitaltwin/android/DigitalTwinServiceClientAndroidRunner.java
@@ -8,28 +8,14 @@ package com.microsoft.azure.sdk.iot.digitaltwin.android;
 import com.microsoft.appcenter.espresso.Factory;
 import com.microsoft.appcenter.espresso.ReportHelper;
 import com.microsoft.azure.sdk.iot.digitaltwin.android.helper.TestGroupDigitalTwin1;
-import com.microsoft.azure.sdk.iot.digitaltwin.e2e.tests.DigitalTwinCommandE2ETests;
-import com.microsoft.azure.sdk.iot.digitaltwin.e2e.tests.DigitalTwinPropertiesE2ETests;
-import com.microsoft.azure.sdk.iot.digitaltwin.e2e.tests.DigitalTwinRegisterInterfacesE2ETests;
 import com.microsoft.azure.sdk.iot.digitaltwin.e2e.tests.DigitalTwinServiceClientE2ETests;
-import com.microsoft.azure.sdk.iot.digitaltwin.e2e.tests.DigitalTwinTelemetryE2ETests;
-import com.microsoft.azure.sdk.iot.digitaltwin.e2e.tests.DigitalTwinTelemetryParameterizedE2ETests;
 
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.rules.Timeout;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
 @TestGroupDigitalTwin1
-@RunWith(Suite.class)
-@Suite.SuiteClasses( {DigitalTwinServiceClientE2ETests.class,
-        DigitalTwinRegisterInterfacesE2ETests.class,
-        DigitalTwinTelemetryE2ETests.class,
-        DigitalTwinTelemetryParameterizedE2ETests.class,
-        DigitalTwinPropertiesE2ETests.class,
-        DigitalTwinCommandE2ETests.class})
-public class AndroidTestSuite {
+public class DigitalTwinServiceClientAndroidRunner extends DigitalTwinServiceClientE2ETests{
     @Rule
     public Timeout globalTimeout = Timeout.seconds(5 * 60); // 5 minutes max per method tested
 
